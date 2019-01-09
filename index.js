b9cb646bcca1b0a26b9c757d4d17aecf1b0b8ac6
kushkamisha <kushka.misha@gmail.com>
2019-01-09 21:48:55 +0200

    Try choose code architecture

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 const request = require('request')
-const $ = require('cheerio')
+const cheerio = require('cheerio')
 
-const url = 'http://orlypark.com.ua/'
+const isEmptyArray = arr => Array.isArray(arr) && arr.length === 0
 
-function Website(url) {
-    this.url = url
-    this.toProcess = new Set()
+const getUrl = () => {
+    const args = process.argv.slice(2)
+    const command = isEmptyArray(args) ? '' : args.shift()
+    const defaultUrl = 'http://orlypark.com.ua'
+    const params = isEmptyArray(args) ? [defaultUrl] : args
+    let url = ''
+
+    const cmds = {
+        URL: '-url'
+    }
+    
+    switch (command) {
+    case cmds.URL:
+        url = params[0]
+        break
+    default:
+        url = defaultUrl
+    }
+
+    return url
 }
 
-const website = new Website(url)
+class Website {
+    constructor() {
+        const url = getUrl()
+        this.url = url.substr(-1) === '/' ? url.slice(0, -1) : url
+        this.toProcess = new Set()
+    }
+
+    isMenuUrl (url) {
+        if (!url) return false
+        return true
+    }
+    
+    getHref (url) {
+        try {
+            let href = url.attribs.href
+            if (href.length <= 1)
+                return ''
+            if (href[0] === '/')
+                href = this.url.concat(href)
+            return href
+        } catch (err) {
+            return ''
+        }
+    }
+    
+    processUrls (urls) {
+        for (const i in urls) {
+            const href = this.getHref(urls[i])
+            if (this.isMenuUrl(href))
+                this.toProcess.add(href)
+        }
+    }
+}
+
+const website = new Website()
 
 request(website.url)
     .on('error', err => {
@@ -20,14 +73,9 @@ request(website.url)
     })
     .on('data', data => {
         const html = data.toString('utf8')
-        const items = $('a', html)
-        for (const i in items) {
-            try {
-                website.toProcess.add(items[i].attribs.href)
-            } catch(err) {
-                //
-            }
-        }
+        const $ = cheerio.load(html)
+        const urls = $('a')
+        website.processUrls(urls)
     })
     .on('complete', () => {
         console.log(website)
