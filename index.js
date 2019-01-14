6a251bc922776480f4997332847fd1978d2aaa50
kushkamisha <kushka.misha@gmail.com>
2019-01-14 14:20:18 +0200

    Code improvements

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict'
 
 /* eslint-disable no-console */
-const { Website, processWebsite } = require('./lib/urls')
+const { createWebpage, processWebpage } = require('./lib/urls')
 const { getUrl } = require('./lib/utils')
 
 const url = getUrl()
 const homeUrl = url
 
-const website = new Website(url, homeUrl)
+const processed = new Set()
+const webpage = createWebpage(url, homeUrl, processed)
 
-processWebsite(website)
+processWebpage(webpage)
     .then(() => {
-        console.log(website)
-    })
-    .catch(err => {
+        console.log(webpage)
+    }, err => {
         throw err
     })
--- test/urls.js
+++ test/urls.js
@@ -2,34 +2,52 @@
 'use strict'
 
 const expect = require('chai').expect
-const { Website, processWebsite } = require('../lib/urls')
+const { createWebpage, processWebpage } = require('../lib/urls')
 
-describe('processWebsite', () => {
+describe('processwebpage', () => {
 
-    let website = {}
+    let webpage = {}
 
     beforeEach(() => {
         const url = 'http://orlypark.com.ua/'
         const homeUrl = 'http://orlypark.com.ua/'
-        website = new Website(url, homeUrl)
+        webpage = createWebpage(url, homeUrl, new Set())
     })
 
     it('should process http://orlypark.com.ua/ correctly', async () => {
-        const _website = {
+        const _webpage = {
             currentUrl: 'http://orlypark.com.ua/',
             homeUrl: 'http://orlypark.com.ua/',
-            toProcess:  new Set([
-                'http://orlypark.com.ua//menu/',
-                'http://orlypark.com.ua//menu/kuhnya/',
-                'http://orlypark.com.ua//menu/detskoe-menyu/',
-                'http://orlypark.com.ua//menu/sushi/',
-                'http://orlypark.com.ua//menu/deserti/',
-                'http://orlypark.com.ua//menu/bar/'
+            toProcess: new Set([
+                'http://orlypark.com.ua/menu/',
+                'http://orlypark.com.ua/menu/kuhnya/',
+                'http://orlypark.com.ua/menu/detskoe-menyu/',
+                'http://orlypark.com.ua/menu/sushi/',
+                'http://orlypark.com.ua/menu/deserti/',
+                'http://orlypark.com.ua/menu/bar/'
+            ]),
+            processed: new Set([
+                'http://orlypark.com.ua/restaurant/',
+                'http://orlypark.com.ua/hotel/',
+                'http://orlypark.com.ua/hotel/standart/',
+                'http://orlypark.com.ua/hotel/polu-lyuks/',
+                'http://orlypark.com.ua/hotel/lyuks/',
+                'http://orlypark.com.ua/detskij-klub/',
+                'http://orlypark.com.ua/gallery/',
+                'http://orlypark.com.ua/news/',
+                'http://orlypark.com.ua/uslugi/',
+                'http://orlypark.com.ua/reviews/',
+                'http://orlypark.com.ua/kontakti/',
+                'http://orlypark.com.ua/stocks/biznes-vremya-v-orly-park.html',
+                'http://orlypark.com.ua/stocks/spetsialnoe-predlozhenie-dlya-provedeniya-svadeb.html',
+                'https://www.facebook.com/orlypark',
+                'https://twitter.com/orly_park',
+                'http://vk.com/orlypark'
             ])
         }
 
-        await processWebsite(website)
+        await processWebpage(webpage)
 
-        expect(website).to.eql(_website)
+        expect(webpage).to.eql(_webpage)
     })
 })
